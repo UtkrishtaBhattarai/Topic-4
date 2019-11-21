07d76b598a51dca6e619b0d90587f31f5db8f394
xpsl412z <bhattarai.utk1@gmail.com>
2019-11-20 20:27:36 -0600

    Adding List

--- app/src/main/java/com/example/topic4/MainActivity.java
+++ app/src/main/java/com/example/topic4/MainActivity.java
@@ -7,9 +7,21 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    public static String countries[]={"Nepal","India","China","UK","US","Canada"};
+    public static final String countries[]=
+                    {"Nepal","Kathmandu",
+                    "India","New Delhi",
+                    "China","Beijing",
+                    "UK","London",
+                    "US","Washington DC",
+                    "Canada","Ottawa"};
+
+    private Map<String,String> dictionary;
     ListView lvcountry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +29,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lvcountry=findViewById(R.id.lvcountry);
 
+        dictionary =new HashMap<>();
+        for (int i=0;i<countries.length;i+=2)
+        {
+            dictionary.put(countries[i],countries[i+1]);
+        }
+
         ArrayAdapter arrayAdapter=new ArrayAdapter<>(
-                this,android.R.layout.simple_list_item_1,countries
+                this,android.R.layout.simple_list_item_1,new ArrayList<String>(dictionary.keySet())
         );
         lvcountry.setAdapter(arrayAdapter);
     }
